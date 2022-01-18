8a8457a39b227f1a280cedc3391d9dfba0b26d5d
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-01-18 14:13:55 +0100

    added images support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import "../styles/index.scss";
+import hello from '../public/img/fun.jpg';
 
 const App = () => {
     return (
         <>
-          <section className="hero"></section>  
-          <main>
-              <section>
-                  <h1>Hello React</h1>
-              </section>
-          </main>
+        <section className="hero"></section>  
+        <main>
+            <section>
+                <h1>Hello React</h1>
+                <img src={hello} alt="" />
+            </section>
+        </main>
         </>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,34 @@ if ( process.env.NODE_ENV === "production" ) {
 module.exports = {
     mode: mode,
     target: target,
+
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",  // decide whether inline or image, best choice
+                // type: "asset/inline",    // base64, creates javascript bundle of the images. Growth size of bundle, bad for large images, only for icons
+                // type: "asset/resource", 
+                // parser: {
+                //     dataUrlCondition: {
+                //         maxsize: 30 * 1024
+                //     }
+                // }
+            },
+            {
                 test: /\.s?css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"] // this order!!
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader, 
+                        // options: { publicPath: "" }, // not neccessary when no error with mcep
+                    },
+                    "css-loader", 
+                    "postcss-loader", 
+                    "sass-loader"
+                ] // this order!!
             },
             {
                 test: /\.jsx?$/,            //$ ends with .js - rules for js
