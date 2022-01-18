ae7859551232f55ff6f285eda16c09b1735f67f3
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-01-18 10:38:33 +0100

    initial webpack setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const elvenShieldRecipe = {
+    leatherStrips: 2,
+    ironIngot: 1,
+    refinedMoonstone: 4
+}
+console.log('🚀 ~ file: index.js ~ line 6 ~ elvenShieldRecipe', elvenShieldRecipe);
+
